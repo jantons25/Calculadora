16a749ed10ce0cb25407b568129d73989c0e608d
jantons25 <ja73133910@gmail.com>
2024-07-11 14:25:07 -0500

    Se agrega el proyecto

---
+++ src/componentes/BotonClear.jsx
@@ -0,0 +1,11 @@
+import '../estilos/botonClear.css'
+
+const  BotonClear = (props) => (
+  <div 
+    className='boton-clear'
+    onClick={props.manejarClear}>
+    {props.children}
+  </div>
+)
+
+export default BotonClear
---
+++ src/componentes/Pantalla.jsx
@@ -0,0 +1,9 @@
+import '../estilos/pantalla.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+);
+
+export default Pantalla
